bdd2c21341f76a7c86511082f5c888e94c1e1b30
ebonatto <ebonatto.dev@gmail.com>
2023-10-26 19:19:11 -0300

    feat: listTasks adicionado no /tasks

--- src/main/java/com/crudgames/todoservice/controller/TaskController.java
+++ src/main/java/com/crudgames/todoservice/controller/TaskController.java
@@ -1,4 +1,25 @@
 package com.crudgames.todoservice.controller;
 
+import com.crudgames.todoservice.Task;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class TaskController {
+    List<Task> taskList = new ArrayList<>();
+
+    // Lista todas as tasks
+    @GetMapping("/tasks")
+    public String listTasks() {
+        StringBuilder allTasks = new StringBuilder();
+        for (Task task : taskList) {
+            allTasks.append(String.format("%s\n", task));
+        }
+        return allTasks.toString();
+    }
+
 }
