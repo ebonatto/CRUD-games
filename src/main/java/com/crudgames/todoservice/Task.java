571fba4e70c75415dacf3f149dd0a15db547c8f9
ebonatto <ebonatto.dev@gmail.com>
2023-10-26 19:18:50 -0300

    feat: toString

--- src/main/java/com/crudgames/todoservice/Task.java
+++ src/main/java/com/crudgames/todoservice/Task.java
@@ -3,10 +3,18 @@ package com.crudgames.todoservice;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data @AllArgsConstructor
+@Data
+@AllArgsConstructor
 public class Task {
-    public int id;
+    public String id;
     public String description;
     public String expiringDate;
     public boolean done;
+
+    @Override
+    public String toString() {
+        return String.format("ID: %s - Até %s - Concluída: %s\n" +
+                        "\tDescrição: %s",
+                this.id, this.expiringDate, (this.done ? "Sim" : "Não"), this.description);
+    }
 }
