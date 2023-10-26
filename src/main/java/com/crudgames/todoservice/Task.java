d97e3155e8cfe9450d66183bee57187c8c210d4d
ebonatto <ebonatto.dev@gmail.com>
2023-10-26 19:41:46 -0300

    refactor: mais um /n no toString

--- src/main/java/com/crudgames/todoservice/Task.java
+++ src/main/java/com/crudgames/todoservice/Task.java
@@ -1,15 +1,20 @@
 package com.crudgames.todoservice;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Task {
     public String id;
     public String description;
     public String expiringDate;
     public boolean done;
+
+    public Task(String id, String description, String expiringDate, boolean done) {
+        this.id = id;
+        this.description = description;
+        this.expiringDate = expiringDate;
+        this.done = done;
+    }
 
     @Override
     public String toString() {
