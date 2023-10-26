680094c2ad1691e1e1ec3e85b8343e9d7e056e29
ebonatto <ebonatto.dev@gmail.com>
2023-10-26 19:42:02 -0300

    feat: addTaks adicionado

--- src/main/java/com/crudgames/todoservice/controller/TaskController.java
+++ src/main/java/com/crudgames/todoservice/controller/TaskController.java
@@ -4,22 +4,39 @@ import com.crudgames.todoservice.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class TaskController {
-    List<Task> taskList = new ArrayList<>();
+//    List<Task> taskList = new ArrayList<>();
+    List<Task> taskList = new ArrayList<>() {{
+        add(new Task("1", "tarefa 1", "01/01/01", false));
+        add(new Task("2", "tarefa 2", "02/02/02", true));
+}};
 
     // Lista todas as tasks
     @GetMapping("/tasks")
     public String listTasks() {
         StringBuilder allTasks = new StringBuilder();
         for (Task task : taskList) {
-            allTasks.append(String.format("%s\n", task));
+            allTasks.append(String.format("%s\n\n", task));
         }
         return allTasks.toString();
     }
 
+
+    // Adiciona tarefa
+    @GetMapping("/tasks/add")
+    public String addTask(
+            @RequestParam(value = "id") String id,
+            @RequestParam(value = "description") String description,
+            @RequestParam(value = "expiringDate") String expiringDate,
+            @RequestParam(value = "status") boolean status
+    ) {
+        taskList.add(new Task(id,description,expiringDate,status));
+        return "Tarefa adicionada!";
+    }
 }
