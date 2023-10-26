978f18c867d7663eab10c059d9f639cea35434f7
ebonatto <ebonatto.dev@gmail.com>
2023-10-26 18:59:08 -0300

    feat: adicionando anotações do Lombok

--- src/main/java/com/crudgames/todoservice/Task.java
+++ src/main/java/com/crudgames/todoservice/Task.java
@@ -1,5 +1,9 @@
 package com.crudgames.todoservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
 public class Task {
     public int id;
     public String description;
