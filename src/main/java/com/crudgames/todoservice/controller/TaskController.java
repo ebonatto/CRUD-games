7f3547b986860144fedfef1aad50ba7510494736
ebonatto <ebonatto.dev@gmail.com>
2023-10-28 17:50:09 -0300

    refactor: alterando add task para POST

--- src/main/java/com/crudgames/todoservice/controller/TaskController.java
+++ src/main/java/com/crudgames/todoservice/controller/TaskController.java
@@ -2,20 +2,18 @@ package com.crudgames.todoservice.controller;
 
 import com.crudgames.todoservice.Task;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TaskController {
-//    List<Task> taskList = new ArrayList<>();
+    //    List<Task> taskList = new ArrayList<>();
     List<Task> taskList = new ArrayList<>() {{
         add(new Task("1", "tarefa 1", "01/01/01", false));
         add(new Task("2", "tarefa 2", "02/02/02", true));
-}};
+    }};
 
     // Lista todas as tasks
     @GetMapping("/tasks")
@@ -29,14 +27,9 @@ public class TaskController {
 
 
     // Adiciona tarefa
-    @GetMapping("/tasks/add")
-    public String addTask(
-            @RequestParam(value = "id") String id,
-            @RequestParam(value = "description") String description,
-            @RequestParam(value = "expiringDate") String expiringDate,
-            @RequestParam(value = "status") boolean status
-    ) {
-        taskList.add(new Task(id,description,expiringDate,status));
+    @PostMapping("/tasks/add")
+    public String addTask(@RequestBody Task task) {
+        taskList.add(task);
         return "Tarefa adicionada!";
     }
 }
